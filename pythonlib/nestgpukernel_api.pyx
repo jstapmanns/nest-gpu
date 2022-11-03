0fa776cb0a0ba9aa26defb31b333b3ecead70ba9
Jonas Stapmanns <jonas.stapmanns@rwth-aachen.de>
2022-11-03 18:20:24 +0100

    added return to create and fixed cmake bug.

--- pythonlib/nestgpukernel_api.pyx
+++ pythonlib/nestgpukernel_api.pyx
@@ -1,9 +1,6 @@
 from libcpp.string cimport string
 from libcpp.vector cimport vector
 
-#from nestgpukernel_api cimport NESTGPU_Connect
-#from nestgpukernel_api cimport NESTGPU_Create
-
 def llapi_connect(int i_source_node, int i_target_node,
 			unsigned char port, unsigned char syn_group,
             float weight, float delay):
@@ -13,6 +10,6 @@ def llapi_connect(int i_source_node, int i_target_node,
 
 def llapi_create(model, long n, int n_port):
     print('using cython llapi_create() to create {} {}(s)'.format(n, model))
-    NESTGPU_Create(model.encode('utf-8'), n, n_port)
+    return NESTGPU_Create(model.encode('utf-8'), n, n_port)
 
 # TODO: should we change all the char* arguments to string as it is done in NEST?
